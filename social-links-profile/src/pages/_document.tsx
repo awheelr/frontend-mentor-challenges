2511c4ca5b3c35431adfc1ddffc335544cba41fb
Austin Wheeler <austinskullfinder@gmail.com>
2025-03-21 20:17:08 -0400

    fix: title and icon

--- social-links-profile/src/pages/_document.tsx
+++ social-links-profile/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon.png" />
+        <title>Frontend Mentor | Social links profile</title>
+      </Head>
       <body className="antialiased bg-(--grey-900)">
         <Main />
         <NextScript />
